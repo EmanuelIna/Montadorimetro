7cd0fa8ba7fcca24877f28cfd20083f5f456844c
emanuel <emanuelinacio052@gmail.com>
2026-07-12 11:18:32 -0400

    Correction from initial commit

---
+++ API/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT;
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  throw new Error("Faltam variáveis de ambiente do Supabase no arquivo .env");
+}
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+);
+
+//Busca dos serviços contados no banco de dados
+app.get("/servico", async (req, res) => {
+  const { data, error } = await supabase.from("servico").select("*");
+
+  if (error) {
+    console.error("Erro no Supabase:", error);
+    return res.status(500).json({ error: error.message });
+  }
+
+  return res.status(200).json(data);
+});
+
+//Criação de um novo serviço no banco de dados
+app.post("/servico", async (req, res) => {
+  const { nome_cliente, data_inicio, data_fim, bandeira } = req.body;
+
+  if (!nome_cliente || !data_inicio || !data_fim || !bandeira) {
+    return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+  }
+
+  const { data, error } = await supabase
+    .from("servico")
+    .insert([{ nome_cliente, data_inicio, data_fim, bandeira }])
+    .select();
+
+  if (error) {
+    console.error("Erro no Supabase:", error);
+    return res.status(500).json({ error: error.message });
+  }
+
+  return res.status(201).json({ message: "Serviço criado com sucesso.", data });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
