7fa9bc17cfbdc35b68817d061a6b015a062223ef
emanuel <emanuelinacio052@gmail.com>
2026-07-14 21:00:14 -0400

    Pequena reformulacao na estrutura de arquivos

---
+++ Front-end/src/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import Form from "./Form";
+import ListaServicos from "./ListaServicos";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Form />
+      <ListaServicos />
+    </div>
+  );
+};
+
+export default Dashboard;
